12f66a9a716a4e94d5bc620293f3d11f82cfd863
Muhammad Damar Kusumo <125840036+DamarKusumo@users.noreply.github.com>
2024-05-17 02:56:46 +0700

    Karir.com Scrapper (#4)
    Co-authored-by: Damar-Kusumo <muhammad.damar01@ui.ac.id>

--- src/scrapper/karir.ts
+++ src/scrapper/karir.ts
@@ -1,56 +1,117 @@
-import { By, until } from "selenium-webdriver";
+import { By, until, Key } from "selenium-webdriver";
 import { DataObject } from "./interface";
-import { saveData, initDriver, consoleData } from "./utils";
+import { saveData, initDriver, consoleData, sleep } from "./utils";
 
-const baseURL = 'https://karir.com/search-lowongan?keyword=';
+const baseURL = 'https://karir.com/search-lowongan';
 
 export const karirRunner = async () => {
-    await save("Programmer");
-    await save("Data");
-    await save("Network");
-    await save("Cyber Security");
+    let res: DataObject[] = [];
+    const driver = await initDriver(baseURL);
+
+    const programmer = await searchByJob("Programmer", driver);
+    if (programmer !== undefined) for (const d of programmer) res.push(d);
+    const data = await searchByJob("Data", driver);
+    console.log(data?.length);
+    if (data !== undefined) for (const d of data) res.push(d);
+    const network = await searchByJob("Network", driver);
+    if (network !== undefined) for (const d of network) res.push(d);
+    const cyberSecurity = await searchByJob("Cyber Security", driver);
+    if (cyberSecurity !== undefined) for (const d of cyberSecurity) res.push(d);
+    driver.quit();
+
+    consoleData(res);
+    return res.length;
 }
 
-export const save = async (job: string) => {
-    const driver = await initDriver(baseURL + job);
+const searchByJob = async (job: string, driver: any) => {
     let data: DataObject[] = [];
-    await driver.wait(until.elementLocated(By.xpath("//div/div[2]/div[3]/div/div/div")), 10000);
-    const outerDiv = await driver.findElement(By.xpath("//div/div[2]/div[3]/div/div/div"));
-
-    const containers = await outerDiv.findElements(By.className("info-company-button-stack"));
-
-    for (const container of containers) {
-        let temp: DataObject = {
-            id: "",
-            title: "",
-            publicationDate: "",
-            location: "",
-            company: "",
-            sourceSite: "Karir.com",
-            linkDetail: "",
-            position: "",
-            logoImgLink: "",
-        }
-        temp.title = await container.findElement(By.css(".info-company-stack p:nth-child(1)")).getText();
-        temp.company = await container.findElement(By.css(".info-company-stack p:nth-child(2)")).getText();
-        temp.location = await container.findElement(By.css(".info-company-stack p:nth-child(4)")).getText();
-        const bottomStackElement = await container.findElement(By.xpath("following-sibling::div[contains(@class, 'bottom-stack')]"));
-        // temp.deadline = await bottomStackElement.findElement(By.css(".MuiTypography-root")).getText();
-        await container.click();
-        // await driver.wait(until.elementTextIs(driver.findElement(By.css('.MuiTypography-root.MuiTypography-body1')), temp.title), 10000);
-
-
-        // Print job information
-        console.log(temp);
-        await driver.wait(until.elementLocated(By.id("job-header-wrapper")), 10000);
-        const detail = await outerDiv.findElement(By.id("job-header-wrapper"));
-       
-        // console.log(text);
-        console.log("------------");
-
-        data.push(temp);
+    const searchField = await driver.findElement(By.xpath("//div/input"));
+    await searchField.sendKeys(Key.chord(Key.CONTROL, "a")); 
+    await searchField.sendKeys(Key.BACK_SPACE);
+    await searchField.sendKeys(job);
+    await driver.findElement(By.xpath("//button[contains(.,'Cari')]")).click();
+    await driver.wait(async () => {
+        const currentUrl = await driver.getCurrentUrl();
+        return currentUrl === (baseURL + `?keyword=${encodeURIComponent(job)}`);
+    }, 10000, 'URL did not match expected value');
+    try {
+        await driver.wait(until.elementLocated(By.className("pagination")), 10000);
+    } catch {
+        return
     }
-    driver.quit();
-    consoleData(data);
-    // saveData(data);
+    const pagination = await driver.findElement(By.className("pagination"));
+    const pages = await pagination.findElements(By.xpath(".//div"));
+    const pagesLen = pages.length;
+    for (let i = 0; i < pagesLen; i++) {
+        const nextPage = pages[i];
+        await nextPage.click();
+        await sleep(2000);
+
+        const containers = await driver.findElements(By.className("info-company-button-stack"));
+        const contLen = containers.length;
+
+        for (let j = 0; j < contLen; j++) {
+            let temp: DataObject = {
+                id: "",
+                title: "",
+                publicationDate: "",
+                location: "",
+                company: "",
+                sourceSite: "Karir.com",
+                linkDetail: baseURL + `?keyword=${job}`,
+                position: job,
+                logoImgLink: "",
+            }
+            await containers[j].click();
+
+            await driver.wait(until.elementLocated(By.xpath("(//p[@type='Body2'])[6]")), 10000);
+            const strPubDate = (await driver.findElement(By.xpath("(//p[@type='Body2'])[6]")).getText());
+            const formatedDate = formatDate(strPubDate);
+            if (!formatedDate) continue;
+            temp.publicationDate = formatedDate
+
+            const title = await containers[j].findElement(By.css(".info-company-stack p:nth-child(1)")).getText();
+            temp.title = title;
+            const company = await containers[j].findElement(By.css(".info-company-stack p:nth-child(2)")).getText();
+            temp.company = company;
+            temp.id = `${title}-${company}-karir`;
+            temp.location = await containers[j].findElement(By.css(".info-company-stack p:nth-child(4)")).getText();
+            const pictElement = await driver.findElement(By.xpath(`(//img[@alt='job image'])[${j + 1}]`));
+            temp.logoImgLink = await pictElement.getAttribute("src");
+
+            data.push(temp);
+        }
+    }
+    
+    return data;
+}
+
+const formatDate = (strDate: string) => {
+    const totalDate = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    if (year % 4 === 0) totalDate[1] = 29;
+    const lstStr = strDate.split(" ");
+    const strNumber = parseInt(lstStr[1]);
+    const strTime = lstStr[2];
+    if (strTime.toLowerCase() === "hari") {
+        if (day > strNumber) day -= strNumber;
+        else {
+            day += totalDate[month - 2] - strNumber;
+            month -= 1;
+        }
+    } else if (strTime.toLowerCase() === "bulan") {
+        if (strNumber > 2) return null
+        if (month > 2) month -= strNumber;
+        else {
+            month += 12 - strNumber;
+            year -= 1;
+        }
+    } else if (strTime.toLowerCase() === "tahun") {
+        return null
+    }
+
+    return `${year}-${month}-${day}`
 }
